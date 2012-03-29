7d3bf91b21379151874066796041e4217ed3ece7
Vinicius Uzeda <vruzeda@gmail.com>
2012-03-28 23:20:47 -0300

    Adding INVENTORY command.

---
+++ src/main/java/com/smud/model/command/informative/InventoryCommand.java
@@ -0,0 +1,55 @@
+package com.smud.model.command.informative;
+
+import java.util.List;
+import java.util.Properties;
+
+import javax.annotation.Resource;
+
+import com.smud.model.Color;
+import com.smud.model.Inventory;
+import com.smud.model.Item;
+import com.smud.model.character.Player;
+import com.smud.model.command.Command;
+import com.smud.model.command.CommandResponse;
+import com.smud.model.command.Response;
+
+public class InventoryCommand implements Command {
+
+	private static final String COMMAND_NAME = "inventory";
+	
+	@Resource(name="textProperties")
+	private Properties textProperties;
+
+	@Override
+	public CommandResponse execute(Player player, String parameters) {
+		CommandResponse commandResponse = new CommandResponse();
+		Response response = new Response("You are carrying:", Color.WHITE);
+		commandResponse.addResponse(response);
+		
+		Inventory inventory = player.getInventory();
+		List<Item> inventoryItems = inventory.getItems();
+		
+		if (inventoryItems.size() > 0) {
+			for (Item item : inventoryItems) {
+				String itemName = textProperties.getProperty("item." + item.getId() + ".name");
+				Response itemResponse = new Response(itemName, Color.WHITE);
+				commandResponse.addResponse(itemResponse);
+			}
+		} else {
+			Response nothingMessage = new Response("Nothing.", Color.WHITE);
+			commandResponse.addResponse(nothingMessage);
+		}
+		
+		return commandResponse;
+	}
+	
+	@Override
+	public String getCommandName() {
+		return COMMAND_NAME;
+	}
+	
+	public void setTextProperties(Properties textProperties) {
+		this.textProperties = textProperties;
+	}
+
+}
---
+++ src/test/java/com/smud/model/command/informative/InventoryCommandTest.java
@@ -0,0 +1,69 @@
+package com.smud.model.command.informative;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.smud.model.Color;
+import com.smud.model.Inventory;
+import com.smud.model.Item;
+import com.smud.model.Keywords;
+import com.smud.model.character.Player;
+import com.smud.model.command.CommandResponse;
+import com.smud.model.command.Response;
+
+public class InventoryCommandTest {
+
+	private static final String INVENTORY_DESCRIPTION = "You are carrying:";
+	private static final String INVENTORY_EMPTY = "Nothing.";
+	
+	private InventoryCommand command;
+	
+	@Before
+	public void setUp() {
+		this.command = new InventoryCommand();
+	}
+	
+	@Test
+	public void testExecuteWithEmptyInventory() throws Exception {
+		Player player = new Player();
+		player.setInventory(new Inventory());
+
+		CommandResponse expectedResult = new CommandResponse();
+		expectedResult.addResponse(new Response(INVENTORY_DESCRIPTION, Color.WHITE));
+		expectedResult.addResponse(new Response(INVENTORY_EMPTY, Color.WHITE));
+		
+		CommandResponse result = this.command.execute(player, null);
+		Assert.assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void testExecuteWithNonEmptyInventory() throws Exception {
+		Keywords keywords = new Keywords();
+		keywords.setWords(Arrays.asList("item"));
+		Item item = new Item();
+		item.setId(1);
+		item.setKeywords(keywords);
+		
+		Player player = new Player();
+		Inventory inventory = new Inventory();
+		inventory.addItem(item);
+		player.setInventory(inventory);
+
+		Properties textProperties = new Properties();
+		textProperties.put("item.1.name", "item name");
+		command.setTextProperties(textProperties);
+		
+		CommandResponse expectedResult = new CommandResponse();
+		expectedResult.addResponse(new Response(INVENTORY_DESCRIPTION, Color.WHITE));
+		expectedResult.addResponse(new Response("item name", Color.WHITE));
+		
+		CommandResponse result = this.command.execute(player, null);
+		Assert.assertEquals(expectedResult, result);
+	}
+	
+}
